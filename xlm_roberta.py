b3120ee7e91b8ba38cfad0faa659c7ac86d06b27
apurvakalia <65355231+apurvakalia@users.noreply.github.com>
2021-04-11 16:22:02 -0400

    Update xlm_roberta.py

--- xlm_roberta.py
+++ xlm_roberta.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import pickle
 import torch
 from transformers import XLMRobertaTokenizer, XLMRobertaModel
+#test
 
 def test_pretrained():
     tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-large")
@@ -53,6 +54,18 @@ def get_pretokenized_text(sentence):
         pretokenized_text.append(sentence[i]['token'])
     return pretokenized_text
 
+def collapse_weights(attn_weights, tok_map):
+    collapsed_weights= np.zeros((attn_weights.shape[0], len(tok_map)))
+    for word_i in range(len(tok_map)):
+        toks = tok_map[word_i]
+        start_cnt = int(np.sum([len(l) for l in tok_map[:word_i]]))
+        word_weight = 0.0
+        for cnt in range(len(toks)):
+            word_weight += attn_weights[:,start_cnt+cnt]
+        collapsed_weights[:,word_i] = word_weight/len(toks)
+        
+    return collapsed_weights
+
 if __name__ == '__main__':
     sentences_english = collect_PUD('English', 'en')
     # sentences_italian = collect_PUD('Italian', 'it')
@@ -65,12 +78,29 @@ if __name__ == '__main__':
 
     try:
         for sentence in tqdm(sentences_english):
-            input_ids = torch.tensor([tokenizer(get_pretokenized_text(sentence), is_split_into_words=True)['input_ids']])
-            outputs = model(input_ids, output_hidden_states=True, output_attentions=False)
+            pre_tok_sen = get_pretokenized_text(sentence)
+            input_ids = torch.tensor([tokenizer(pre_tok_sen, is_split_into_words=True)['input_ids']])
+            tok_map = [tokenizer.encode(x, add_special_tokens=False) for x in pre_tok_sen]
+            outputs = model(input_ids, output_hidden_states=True, output_attentions=True)
 
             for word_i in range(1, len(sentence)):
+                toks = tok_map[word_i-1]
+                start_cnt = int(np.sum([len(l) for l in tok_map[:word_i-1]]))+1
+                #print("at word {}".format(word_i))
                 for emb_i in range(len(outputs.hidden_states)):
-                    sentence[word_i]['Layer %d' % (emb_i)] = outputs.hidden_states[emb_i][0, word_i, :].detach().numpy()
+                    sentence[word_i]['Layer %d' % (emb_i)] = 0.0
+                    for cnt in range(len(toks)):
+                        sentence[word_i]['Layer %d' % (emb_i)] += outputs.hidden_states[emb_i][0, start_cnt+cnt, :].detach().numpy()
+                    sentence[word_i]['Layer %d' % (emb_i)] /= len(toks)
+                #print("Attn for word {}".format(word_i))
+                for emb_i in range(len(outputs.attentions)):
+                    attn_weights = 0.0
+                    for cnt in range(len(toks)):
+                        attn_weights += outputs.attentions[emb_i][0, :, start_cnt+cnt, :].detach().numpy()
+                    attn_weights /= len(toks)
+                    attn_weights = collapse_weights(attn_weights, tok_map)
+                    sentence[word_i]['Attention Layer %d' % (emb_i)] = attn_weights
+                    
     except:
         print(sentence[0]['text'])
         print(len(sentence))
